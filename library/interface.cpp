d71c43db92d5a8b5d649392abc3d3394bee2397c
Dean De Leo <deandeleo@hotmail.co.uk>
2020-12-14 16:39:59 +0100

    Restore the version to Teseo v8

--- library/interface.cpp
+++ library/interface.cpp
@@ -272,11 +272,11 @@ vector<ImplementationManifest> implementations() {
     // v6 18/07/2020: vertex table
     // v7 08/11/2020: vertical partitioning
     // v8 23/11/2020: variable length leaves
-    result.emplace_back("teseo.7", "Teseo", &generate_teseo);
-    result.emplace_back("teseo-rw.7", "Teseo. Use read-write transactions for graphalytics, to measure their overhead", &generate_teseo_rw);
-    result.emplace_back("teseo-lcc.7", "Teseo with a tuned implementation of the LCC kernel", &generate_teseo_lcc);
-    result.emplace_back("teseo-dv.7", "Teseo, dense vertices", &generate_teseo_real_vtx);
-    result.emplace_back("teseo-lcc-dv.7", "Teseo, dense vertices and sort-merge implementation of the LCC kernel", &generate_teseo_real_vtx_lcc);
+    result.emplace_back("teseo.8", "Teseo", &generate_teseo);
+    result.emplace_back("teseo-rw.8", "Teseo. Use read-write transactions for graphalytics, to measure their overhead", &generate_teseo_rw);
+    result.emplace_back("teseo-lcc.8", "Teseo with a tuned implementation of the LCC kernel", &generate_teseo_lcc);
+    result.emplace_back("teseo-dv.8", "Teseo, dense vertices", &generate_teseo_real_vtx);
+    result.emplace_back("teseo-lcc-dv.8", "Teseo, dense vertices and sort-merge implementation of the LCC kernel", &generate_teseo_real_vtx_lcc);
 #endif
 
     return result;
